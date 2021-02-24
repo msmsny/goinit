ceb65626efaf726da49e8e82b27d5c86a5b068f6
msmsny <msmsny@gmail.com>
2021-02-25 00:46:42 +0900

    add test code

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/msmsny/goinit
 
 go 1.16
 
-require github.com/rakyll/statik v0.1.7
+require (
+	github.com/rakyll/statik v0.1.7
+	github.com/stretchr/testify v1.7.0
+)
---
+++ internal/goinit/goinit_test.go
@@ -0,0 +1,88 @@
+package goinit
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"text/template"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRun(t *testing.T) {
+	wd, err := os.Getwd()
+	assert.NoError(t, err)
+
+	repo := "path/to/repo"
+	name := "foo"
+	subName := "bar"
+	appDir := "internal"
+	ttpl := template.New("goinit")
+
+	testCases := map[string]struct {
+		goinit *goinit
+		paths  map[string]string
+	}{
+		"main": {
+			goinit: &goinit{
+				params: &templateParams{
+					Repo:      repo,
+					Name:      name,
+					UpperName: strings.Title(name),
+					AppDir:    appDir,
+				},
+				tpl: ttpl,
+			},
+			paths: map[string]string{
+				"template/cmd/name/name.go.txt":                     fmt.Sprintf("cmd/%s/%s.go", name, name),
+				"template/internal/cmd/name/name.go.txt":            fmt.Sprintf("%s/cmd/%s/%s.go", appDir, name, name),
+				"template/internal/cmd/name/options/options.go.txt": fmt.Sprintf("%s/cmd/%s/options/options.go", appDir, name),
+				"template/internal/name/name.go.txt":                fmt.Sprintf("%s/%s/%s.go", appDir, name, name),
+			},
+		},
+		"sub": {
+			goinit: &goinit{
+				params: &templateParams{
+					Repo:         repo,
+					Name:         name,
+					UpperName:    strings.Title(name),
+					SubName:      subName,
+					UpperSubName: strings.Title(subName),
+					AppDir:       appDir,
+				},
+				tpl: ttpl,
+			},
+			paths: map[string]string{
+				"template/cmd/name/name.go.txt":                     fmt.Sprintf("cmd/%s/%s.go", name, name),
+				"template/internal/cmd/name/name.go.txt":            fmt.Sprintf("%s/cmd/%s/%s.go", appDir, name, name),
+				"template/internal/cmd/name/options/options.go.txt": fmt.Sprintf("%s/cmd/%s/options/options.go", appDir, name),
+				"template/internal/cmd/name/sub/sub.go.txt":         fmt.Sprintf("%s/cmd/%s/%s/%s.go", appDir, name, subName, subName),
+				"template/internal/name/name.go.txt":                fmt.Sprintf("%s/%s/%s.go", appDir, name, name),
+			},
+		},
+	}
+
+	for testCase, tt := range testCases {
+		t.Run(testCase, func(t *testing.T) {
+			tempDir := t.TempDir()
+			assert.NoError(t, tt.goinit.run(tempDir))
+
+			for tplPath, path := range tt.paths {
+				rawTpl, err := ioutil.ReadFile(filepath.FromSlash(wd + "/" + tplPath))
+				assert.NoError(t, err)
+				tpl, err := tt.goinit.tpl.Parse(string(rawTpl))
+				assert.NoError(t, err)
+				wantCode := &bytes.Buffer{}
+				assert.NoError(t, tpl.Execute(wantCode, tt.goinit.params))
+
+				gotCode, err := ioutil.ReadFile(filepath.FromSlash(tempDir + "/" + path))
+				assert.NoError(t, err)
+				assert.Equal(t, wantCode.Bytes(), gotCode)
+			}
+		})
+	}
+}
